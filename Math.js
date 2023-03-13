28d80d0be601a6cc4218abffc0616fa020e777e7
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-03-13 16:25:47 +0700

    create Math.js

---
+++ Math.js
@@ -0,0 +1,11 @@
+const add = (a,b) => a+b;
+const substract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+console.log(add(1,5));
+console.log(substract(1,5));
+console.log(multiply(1,5));
+console.log(divide(1,5));
+
+
